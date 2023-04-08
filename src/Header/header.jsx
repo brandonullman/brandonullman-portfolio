3bfe3473239cf5d654173f163b2e16f9b49805a9
Brandon <ullman.brandon@gmail.com>
2023-04-07 20:13:42 -0700

    added picture and changed colors

--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { HomeIcon, ResumeIcon, ProjectsIcon, ContactIcon, GithubIcon, LinkedinIcon, SunIcon, MoonIcon, MenuOpenIcon, MenuCloseIcon } from './../platformicons';
+import brandonPicture from './../brandon-picture.jpg';
 
 const NavItem = ({ icon, text }) => {
   return (
@@ -12,14 +13,15 @@ const NavItem = ({ icon, text }) => {
 
 const Sidebar = () => {
   return (
-    <div className="w-40 shadow-lg z-50 relative bg-[#3C665B]">
-      <nav className="flex flex-col p-6 text-[#FFD3A7] text-lg gap-6">
+    <div className="shadow-lg z-50 relative bg-[#2d3868] w-52">
+      <img src={brandonPicture} alt="Brandon Headshot" className="rounded-full w-28 h-28 mx-auto mt-6" />
+      <nav className="flex flex-col text-[#FFD3A7] text-lg mt-8 ml-14 gap-6">
         <NavItem icon={<HomeIcon color="#FFD3A7" />} text='Home' />
         <NavItem icon={<ResumeIcon color="#FFD3A7" />} text='Resume' />
         <NavItem icon={<ProjectsIcon color="#FFD3A7" />} text='Projects' />
         <NavItem icon={<ContactIcon color="#FFD3A7" />} text='Contact' />
       </nav>
-      <div className="absolute bottom-0 w-full flex justify-center gap-8 py-2 bg-zinc-800">
+      <div className="absolute bottom-0 w-full flex justify-center gap-8 py-4 bg-zinc-800">
         <a href="https://github.com/brandonullman" target="_blank"><GithubIcon color="#FFD3A7" /></a>
         <a href="https://www.linkedin.com/in/brandonullman/" target="_blank"><LinkedinIcon color="#FFD3A7" /></a>
       </div>
@@ -42,7 +44,7 @@ const Header = () => {
             className="text-white mr-4"
             onClick={handleToggleSidebar}
           >
-            {showSidebar ? <MenuCloseIcon color="#599787"  /> : <MenuOpenIcon color="#599787"  />}
+            {showSidebar ? <MenuCloseIcon color="#599787" /> : <MenuOpenIcon color="#599787" />}
           </button>
           <h1 className="text-[#FFD3A7] font-bold text-lg">Brandon Ullman</h1>
         </header>
