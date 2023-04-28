63be71f7f1023ce6c1da808174b3ada704cd4ba9
Brandon <ullman.brandon@gmail.com>
2023-04-28 15:21:09 -0700

    contact form styling

--- src/contact.jsx
+++ src/contact.jsx
@@ -32,59 +32,74 @@ const ContactForm = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)} noValidate>
-        <div>
-          <input 
-            type='text' 
-            name='name' 
-            placeholder='Name'
-            {...register('name', {
-              required: { value: true, message: 'Please enter your name' },
-              maxLength: {
-                value: 30,
-                message: 'Please use 30 characters or less'
-              }
-            })}
-          ></input>
-          {errors.name && <span className='errorMessage'>{errors.name.message}</span>}
-          <input 
-            type='text' 
-            name='email' 
-            placeholder='Email Address'
-            {...register('email', {
-              required: true,
-              pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-            })}
-          ></input>
-          {errors.email && (<span className='errorMessage'>Please enter a valid email address</span>)}
-        </div>
-        <input 
-          type='text' 
-          name='subject' 
-          placeholder='Subject'
-          {...register('subject', {
-            required: { value: true, message: 'Please enter a subject' },
-            maxLength: {
-              value: 75,
-              message: 'Subject cannot exceed 75 characters'
-            }
-          })}
-        ></input>
-        {errors.subject && (<span className='errorMessage'>{errors.subject.message}</span>)}
-        <input 
-          type='text' 
-          name='message' 
-          placeholder='Message'
-          {...register('message', {
-            required: true
-          })}
-        ></input>
-        {errors.message && <span className='errorMessage'>Please enter a message</span>}
-        <button type='submit' className="text-[#D6D6D6]">
-          Submit
-        </button>
-      </form>
+    <div className="bg-[#0d2636] mt-40 w-1/2 mx-auto rounded-md">
+      <div className="py-8 px-4 text-center w-3/4 mx-auto">
+        <h1 className="mb-8 text-4xl font-semibold text-[#D6D6D6]">Want to Chat?</h1>
+        <form onSubmit={handleSubmit(onSubmit)} noValidate className="space-y-8">
+          <div className="flex gap-4">
+            <div className="w-full">
+              <input 
+                type='text' 
+                name='name' 
+                placeholder='Name'
+                className={`shadow-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-md w-full ${errors.name && 'border-red-400'}`}
+                {...register('name', {
+                  required: { value: true, message: 'Please enter your name' },
+                  maxLength: {
+                    value: 30,
+                    message: 'Please use 30 characters or less'
+                  }
+                })}
+              ></input>
+              {errors.name && <span className="text-red-400 font-medium">{errors.name.message}</span>}
+            </div>
+            <div className="w-full">
+              <input 
+                type='text' 
+                name='email' 
+                placeholder='Email Address'
+                className={`shadow-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-md w-full ${errors.email && 'border-red-400'}`}
+                {...register('email', {
+                  required: true,
+                  pattern: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+                })}
+              ></input>
+              {errors.email && (<span className="text-red-400 font-medium">Please enter a valid email address</span>)}
+            </div>
+          </div>
+          <div>
+            <input 
+              type='text' 
+              name='subject' 
+              placeholder='Subject'
+              className={`shadow-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-md w-full ${errors.subject && 'border-red-400'}`}
+              {...register('subject', {
+                required: { value: true, message: 'Please enter a subject' },
+                maxLength: {
+                  value: 75,
+                  message: 'Subject cannot exceed 75 characters'
+                }
+              })}
+            ></input>
+            {errors.subject && (<span className="text-red-400 font-medium">{errors.subject.message}</span>)}
+          </div>
+          <div>
+            <textarea 
+              rows="6" 
+              name='message' 
+              placeholder='Message'
+              className={`shadow-sm bg-gray-50 border border-gray-300 text-gray-900 rounded-md w-full ${errors.message && 'border-red-400'}`}
+              {...register('message', {
+                required: true
+              })}
+            ></textarea>
+            {errors.message && <span className="text-red-400 font-medium">Please enter a message</span>}
+          </div>
+          <button type='submit' className="py-3 px-5 text-center text-[#D6D6D6] rounded-lg bg-[#081a25] hover:bg-[#05121a] focus:ring-4 focus:outline-none focus:ring-[#8FF6FF]">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
